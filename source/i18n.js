b80f2b02d4360413b9ec94fa78574181fdd4c908
Ebrahim Byagowi <ebraminio@gmail.com>
2012-01-04 18:59:20 +0800

    Translating to Persian (farsi). "Lorem ipsum" is from wikipedia.

--- source/i18n.js
+++ source/i18n.js
@@ -2,42 +2,51 @@
 this.i18n = {
     "settings": {
         "en": "Settings",
-        "de": "Optionen"
+        "de": "Optionen",
+        "fa": "تنظیمات"
     },
     "search": {
         "en": "Search",
-        "de": "Suche"
+        "de": "Suche",
+        "fa": "جستجو"
     },
     "nothing-found": {
         "en": "No matches were found.",
-        "de": "Keine Übereinstimmungen gefunden."
+        "de": "Keine Übereinstimmungen gefunden.",
+        "fa": "موردی که مطابقت داشته باشد یافت نشد."
     },
     
     
     
     "information": {
         "en": "Information",
-        "de": "Information"
+        "de": "Information",
+        "fa": "اطلاعات"
     },
     "login": {
         "en": "Login",
-        "de": "Anmeldung"
+        "de": "Anmeldung",
+        "fa": "ورود"
     },
     "username": {
         "en": "Username:",
-        "de": "Benutzername:"
+        "de": "Benutzername:",
+        "fa": "نام کاربری:"
     },
     "password": {
         "en": "Password:",
-        "de": "Passwort:"
+        "de": "Passwort:",
+        "fa": "گذرواژه:"
     },
     "x-characters": {
         "en": "6 - 12 characters",
-        "de": "6 - 12 Zeichen"
+        "de": "6 - 12 Zeichen",
+        "fa": "۶ - ۱۲ نویسه"
     },
     "x-characters-pw": {
         "en": "10 - 18 characters",
-        "de": "10 - 18 Zeichen"
+        "de": "10 - 18 Zeichen",
+        "fa": "۱۰ - ۱۸ نویسه"
     },
     "description": {
         "en": "This is a description. You can write any text inside of this.<br>\
@@ -54,18 +63,30 @@ this.i18n = {
         et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem\
         ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et\
         dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.\
-        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+        
+        "fa": "این یک توضیح است. شما می‌توانیدهر متنی را داخل آن بنویسید.<br>\
+        این طرح‌نما می‌آزماید نگاره و اندازه و شکل را.\
+        آیا آن که ما خوانیم ترا برای شایست‌تان را می‌گواهد یا خوانش‌ها را می‌نماید.\
+        نمایان کنم بر شمایان خواهان شدن نشدن و نا شدن و که این برگزیدگان ناگزیده از کدامین توان با ناتوانان\
+        پرتوان شمارش نموده آیا هم‌چنان خواستار ناشمردنی‌ها بوده و هستند. آیا نیستند و چرا؟\
+        این نوشتار آزمایشی چرا و به چه علت از شمایان برمی‌شمارد و از کجا و به کدامین نوا برمی‌نگارد و بازنمی‌ایستاندتان؟\
+        خواهشمندی ناشنودگان از پرمایگی نابیوسیدگان برتر نباد و مایه بی‌مایگی سراسر شایان بادا.\
+        باشد تا آزمون‌ها خوش و آزموده‌ها سرخوش از بوته ناخوشی‌ها سراسر سر بدر آورده با باد همراه روند."
     },
     "logout": {
         "en": "Logout",
-        "de": "Abmeldung"
+        "de": "Abmeldung",
+        "fa": "خروج"
     },
     "enable": {
         "en": "Enable",
-        "de": "Aktivieren"
+        "de": "Aktivieren",
+        "fa": "فعال‌سازی"
     },
     "disconnect": {
         "en": "Disconnect:",
-        "de": "Trennen:"
+        "de": "Trennen:",
+        "fa": "قطع اتصال:"
     }
 };
